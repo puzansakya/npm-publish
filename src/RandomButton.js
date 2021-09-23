2b3c0532c4a8ad494ccd297946db0ec7401f6fa9
Puzan sakya <pujan@rigonepal.com>
2021-09-23 23:32:46 +0545

    initial commit

--- src/RandomButton.js
+++ src/RandomButton.js
@@ -1,17 +1,16 @@
-  
 import React from "react";
 
-const RandomButton = props => {
+const RandomButton = (props) => {
   const { width, height, color, text } = props;
   return (
     <button
       style={{
         width: width || 100,
         height: height || 100,
-        backgroundColor: color || "blue"
+        backgroundColor: color || "blue",
       }}
     >
-      {text}
+      pz {text}
     </button>
   );
 };
